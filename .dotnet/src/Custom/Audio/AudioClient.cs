3ef4e28b3b0289ffdce5a5a23cfb8ad725a6cda6
Anne Thompson <annelo@microsoft.com>
2024-03-13 09:59:25 -0700

    go back to using BCL type

--- .dotnet/src/Custom/Audio/AudioClient.cs
+++ .dotnet/src/Custom/Audio/AudioClient.cs
@@ -3,6 +3,7 @@ using System;
 using System.ClientModel;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;

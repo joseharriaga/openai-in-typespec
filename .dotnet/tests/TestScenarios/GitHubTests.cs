efd8d81b42f440e4a2c788e7e50c88235031f16b
Patrick Hallisey <pahallis@microsoft.com>
2024-03-15 12:47:42 -0700

    Add simple github secret test

---
+++ .dotnet/tests/TestScenarios/GitHubTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace OpenAI.Tests;
+
+public partial class GitHubTests
+{
+    [Test(Description = "Test that we can use a GitHub secret")]
+    [Category("GitHub")]
+    public void CanUseGitHubSecret()
+    {
+        string gitHubSecretString = Environment.GetEnvironmentVariable("SECRET_VARIABLE");
+        Assert.That(gitHubSecretString, Is.Not.Null.Or.Empty);
+    }
+}

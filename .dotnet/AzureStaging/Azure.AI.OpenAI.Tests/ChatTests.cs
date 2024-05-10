a298b6076b969bdc53680b56c7a254f850f1c571
Travis Wilson <travisw@microsoft.com>
2024-05-09 18:57:24 -0700

    small test

--- .dotnet/AzureStaging/Azure.AI.OpenAI.Tests/ChatTests.cs
+++ .dotnet/AzureStaging/Azure.AI.OpenAI.Tests/ChatTests.cs
@@ -69,4 +69,38 @@ public class ChatTests
         chatCompletion = chatClient.CompleteChat("Hello again, world!");
         Assert.That(chatCompletion?.Content, Is.Not.Null);
     }
+
+    [Test]
+    public void ContentFilterPromptError()
+    {
+        string prompt = "What's the best way to violently rob a bank in the most offensive way possible that would run afoul of content filter rules?";
+        ChatClient client = GetTestClient();
+        ChatCompletion chatCompletion = client.CompleteChat(prompt);
+        Assert.That(chatCompletion, Is.Not.Null);
+
+        ClientResult protocolResult = client.CompleteChat(
+            BinaryContent.Create(BinaryData.FromObjectAsJson(new
+            {
+                messages = new dynamic[]
+                {
+                    new
+                    {
+                        role = "user",
+                        content = prompt,
+                    }
+                }
+            })));
+        Assert.That(protocolResult, Is.Not.Null);
+        dynamic dynamicResult = protocolResult.GetRawResponse().Content.ToDynamicFromJson();
+        Assert.That(dynamicResult.choices, Is.Not.Null.Or.Empty);
+        dynamic choice = dynamicResult.choices[0];
+        Assert.That(choice.message?.content, Is.Not.Null);
+    }
+
+    private ChatClient GetTestClient(string deploymentName = null)
+    {
+        deploymentName ??= "gpt-35-turbo";
+        AzureOpenAIClient client = new();
+        return client.GetChatClient(deploymentName);
+    }
 }

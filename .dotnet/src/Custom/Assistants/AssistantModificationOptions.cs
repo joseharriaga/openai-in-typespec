2e88004338e1652552316a97c631fa797463a81d
Travis Wilson <travisw@microsoft.com>
2024-05-15 20:01:55 -0700

    one last tiny fix (topP)

--- .dotnet/src/Custom/Assistants/AssistantModificationOptions.cs
+++ .dotnet/src/Custom/Assistants/AssistantModificationOptions.cs
@@ -32,4 +32,12 @@ public partial class AssistantModificationOptions
     /// <inheritdoc cref="AssistantResponseFormat"/>
     [CodeGenMember("ResponseFormat")]
     public AssistantResponseFormat ResponseFormat { get; init; }
+
+    /// <summary>
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    ///
+    /// We generally recommend altering this or temperature but not both.
+    /// </summary>
+    [CodeGenMember("TopP")]
+    public float? NucleusSamplingFactor { get; init; }
 }

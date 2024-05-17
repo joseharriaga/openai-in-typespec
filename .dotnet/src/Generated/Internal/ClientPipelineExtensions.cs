72d70d1512b535fca52c0a583f1e875562df5c66
Anne Thompson <annelo@microsoft.com>
2024-05-17 12:12:31 -0700

    nits

--- .dotnet/src/Generated/Internal/ClientPipelineExtensions.cs
+++ .dotnet/src/Generated/Internal/ClientPipelineExtensions.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace OpenAI
 {
-    internal  static partial class ClientPipelineExtensions
+    internal static partial class ClientPipelineExtensions
     {
         public static async ValueTask<ClientResult<bool>> ProcessHeadAsBoolMessageAsync(this ClientPipeline pipeline, PipelineMessage message, RequestOptions options)
         {

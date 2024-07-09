e31714f61baec71e463ce884356fb53dd9cc3c7f
Travis Wilson <travisw@microsoft.com>
2024-07-08 17:50:32 -0700

    basic plumbing for BingSearchToolDefinition

---
+++ .dotnet.azure/src/Custom/Assistants/Internal/GeneratorStubs.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.AI.OpenAI.Assistants;
+
+[CodeGenModel("BingSearchToolDefinitionBrowser")] internal partial class InternalBingSearchToolDefinitionBrowser { }

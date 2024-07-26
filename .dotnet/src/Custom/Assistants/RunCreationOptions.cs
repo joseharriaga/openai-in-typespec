a8727492756efd28d526b0a290fcbee66a37a3ff
ShivangiReja <shivangi.reja@microsoft.com>
2024-07-26 15:05:26 -0700

    Feedback

--- .dotnet/src/Custom/Assistants/RunCreationOptions.cs
+++ .dotnet/src/Custom/Assistants/RunCreationOptions.cs
@@ -102,7 +102,7 @@ public partial class RunCreationOptions
     public IDictionary<string, string> Metadata { get; } = new ChangeTrackingDictionary<string, string>();
 
     /// <summary> What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. </summary>
-    public float? Temperature { get; }
+    public float? Temperature { get; set; }
 
     /// <summary>
     /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.

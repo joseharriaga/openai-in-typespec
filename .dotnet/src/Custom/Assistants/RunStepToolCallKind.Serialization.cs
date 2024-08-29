370f530836cd8fc20e96d0823f8fab68e20bbf8c
ShivangiReja <shivangi.reja@microsoft.com>
2024-08-28 17:52:40 -0700

    Remove Experimental from an internal type

--- .dotnet/src/Custom/Assistants/RunStepToolCallKind.Serialization.cs
+++ .dotnet/src/Custom/Assistants/RunStepToolCallKind.Serialization.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace OpenAI.Assistants;
 
-[Experimental("OPENAI001")]
 internal static partial class RunStepToolCallKindExtensions
 {
     public static string ToSerialString(this RunStepToolCallKind value) => value switch

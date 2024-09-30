34043be55e3cf6a747eca922bdb2ff4716423478
Travis Wilson <travisw@microsoft.com>
2024-09-30 09:47:29 -0700

    PR feedback

--- .dotnet.azure/sdk/openai/Azure.AI.OpenAI/src/Custom/Common/Internal/GeneratorStubs.cs
+++ .dotnet.azure/sdk/openai/Azure.AI.OpenAI/src/Custom/Common/Internal/GeneratorStubs.cs
@@ -7,5 +7,6 @@ using System.Diagnostics.CodeAnalysis;
 namespace Azure.AI.OpenAI;
 
 [Experimental("AOAI001")][CodeGenModel("AzureContentFilterBlocklistIdResult")] internal partial class InternalAzureContentFilterBlocklistIdResult { }
+[Experimental("AOAI001")][CodeGenModel("AzureContentFilterBlocklistResultDetail")] internal partial class InternalAzureContentFilterBlocklistResultDetail { }
 [Experimental("AOAI001")][CodeGenModel("AzureContentFilterResultForPromptContentFilterResults")] internal partial class InternalAzureContentFilterResultForPromptContentFilterResults { }
 [Experimental("AOAI001")][CodeGenModel("AzureContentFilterResultForPromptContentFilterResultsError")] internal partial class InternalAzureContentFilterResultForPromptContentFilterResultsError { }

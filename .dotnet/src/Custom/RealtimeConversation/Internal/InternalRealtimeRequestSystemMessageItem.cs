ef989f9c07e1d4cc660f99ecdaaf813009437ee9
Travis Wilson <travisw@microsoft.com>
2024-11-06 10:25:21 -0800

    fix using statements

--- .dotnet/src/Custom/RealtimeConversation/Internal/InternalRealtimeRequestSystemMessageItem.cs
+++ .dotnet/src/Custom/RealtimeConversation/Internal/InternalRealtimeRequestSystemMessageItem.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace OpenAI.RealtimeConversation;
 
--- .dotnet/src/Custom/RealtimeConversation/Internal/InternalRealtimeRequestUserMessageItem.cs
+++ .dotnet/src/Custom/RealtimeConversation/Internal/InternalRealtimeRequestUserMessageItem.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.ClientModel.Primitives;
-using System.Text.Json;
-using System.Linq;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace OpenAI.RealtimeConversation;
 

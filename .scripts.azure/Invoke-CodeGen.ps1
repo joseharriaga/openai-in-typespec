f260e61b783c31b675405eb86623f89aad585d17
Travis Wilson <travisw@microsoft.com>
2025-02-14 11:07:41 -0800

    minor -- correct fresh setup ordering and add parity timing info to script

--- .scripts.azure/Invoke-CodeGen.ps1
+++ .scripts.azure/Invoke-CodeGen.ps1
@@ -6,15 +6,23 @@ function Invoke([scriptblock]$script) {
   & $script
 }
 
+$scriptStartTime = Get-Date
+
 Push-Location $repoRoot
+
 try {
-  Set-Location $repoRoot/.typespec.azure
   Invoke { npm ci }
+  Invoke { npm run build -w .plugin.azure }
+  Set-Location $repoRoot/.typespec.azure
   Invoke { npm exec --no -- tsp format **/*tsp }
   Invoke { npm exec --no -- tsp compile . }
-  Set-Location $repoRoot
-  Invoke { npm run build -w .plugin.azure }
 }
 finally {
   Pop-Location
 }
+
+$scriptElapsed = $(Get-Date) - $scriptStartTime
+$scriptElapsedSeconds = [math]::Round($scriptElapsed.TotalSeconds, 1)
+$scriptName = $MyInvocation.MyCommand.Name
+
+Write-Host "${scriptName} complete. Time: ${scriptElapsedSeconds}s"
